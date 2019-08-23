d48581f7ddc74f53b91016016f22c11200b06111
Prashant Palikhe <prashpal09@gmail.com>
2019-08-23 21:11:21 +0200

    refactor: lint fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const alfy = require('alfy');
 const algoliasearch = require('algoliasearch');
-const { last } = require('lodash');
+const {last} = require('lodash');
 
 const client = algoliasearch('BH4D9OD16A', 'ff80fbf046ce827f64f06e16f82f1401');
 const index = client.initIndex('nuxtjs');
 
 (async () => {
-	const { hits } = await index.search({
+	const {hits} = await index.search({
 		query: alfy.input,
 		hitsPerPage: 5,
 		facetFilters: ['tags:en']
